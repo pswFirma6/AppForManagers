8baf9cd94b239a091507c82dc3adff1486780e08
SeekEquilibrium <markovic.ra107.2018@uns.ac.rs>
2021-11-15 19:33:58 +0100

    fix: fixed merging and implementation issues

--- ApplicationForManagers/src/app/app-routing.module.ts
+++ ApplicationForManagers/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageLayoutComponent } from './homePage/home-page-layout/home-page-layout.component';
+import { LandingpageLayoutComponent } from './landingpage/landingpage-layout/landingpage-layout.component';
+
 
 const routes: Routes = [
   {
     path: '',
+    component: LandingpageLayoutComponent,
+  },
+  {
+    path: 'home',
     component: HomePageLayoutComponent,
     children: [
       {
