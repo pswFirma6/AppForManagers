a5c5b6903fb7a79748841d3fb5fcfbba86ce6d4d
Georgije024 <73826278+Georgije024@users.noreply.github.com>
2021-12-12 23:02:33 +0100

    feat: malicious patients (#41)
    * feat: started work on the display malicious patients
    
    * feat: display malicious patients

---
+++ ApplicationForManagers/src/app/homePage/all-patients/all-patients.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { PatientsService } from 'src/app/service/patients.service';
+import { Patients } from 'src/app/shared/patients';
+
+@Component({
+  selector: 'app-all-patients',
+  templateUrl: './all-patients.component.html',
+  styleUrls: ['./all-patients.component.css']
+})
+export class AllPatientsComponent implements OnInit {
+
+  public patients: Patients[] = []
+  
+  constructor(private patientsService: PatientsService) { }
+
+  ngOnInit(): void {
+    this.patientsService.getMethod().subscribe(res =>{
+      console.log(res)
+      this.patients = res;
+      console.log(this.patients)
+    });
+
+
+}
+}
