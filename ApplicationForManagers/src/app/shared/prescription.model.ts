5288c013072811a77666773ca8614983316657f0
poparicsara <73502384+poparicsara@users.noreply.github.com>
2021-12-23 17:23:38 +0100

    fix: prescription model fixed

--- ApplicationForManagers/src/app/shared/prescription.model.ts
+++ ApplicationForManagers/src/app/shared/prescription.model.ts
@@ -2,12 +2,10 @@ export class PrescriptionModel {
     Id: number = 0;
     MedicineName: string = "";
     Quantity: number = 0;
-    Description: string = "";
     RecommendedDose: string = "";
     PrescriptionDate: string = "";
     DoctorName: string = "";
     PatientName: string = "";
-    PatientId: string = "";
     TherapyStart : string = "";
     TherapyEnd: string = "";
     Diagnosis: string = "";
